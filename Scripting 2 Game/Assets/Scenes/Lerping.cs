097beded13bb3e1c53f7d1c5b21d6cd1a09c48bf
Megan Petersen <petersen.megan9646@gmail.com>
2020-09-18 23:26:42 -0600

    Working on Lerping and LookAt code. Got lost in class. Oof.

---
+++ Scripting 2 Game/Assets/Scenes/Lerping.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerping : MonoBehaviour
+{
+
+
+public Vector3 vOne, vTwo;
+public float value = 2f; 
+
+
+
+    
+    // Update is called once per frame
+   private void Update()
+    {
+
+    var direction = Vector3.Lerp(vOne, vTwo, value);
+    value += 0.1f*Time.deltaTime; 
+    transform.Translate(direction); 
+        
+    }
+}
