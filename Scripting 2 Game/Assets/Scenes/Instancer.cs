24d46113845fecf0d5aedcd7f8becd5ac2b5ef11
Megan Petersen <petersen.megan9646@gmail.com>
2020-09-21 22:44:30 -0600

    Applied Force and may have good solution for Health. Will test it later

---
+++ Scripting 2 Game/Assets/Scenes/Instancer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Instancer : MonoBehaviour
+{
+
+    public GameObject prefab; 
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        var location = transform.position;
+        Instantiate( prefab, location, Quaternion.identity);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
