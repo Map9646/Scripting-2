f742f86b207312e81d65e39ddc61d5514a9c231f
Megan Petersen <petersen.megan9646@gmail.com>
2020-09-28 16:47:02 -0600

    Stuff we did in class. Will do more tonight.

---
+++ Scripting 2 Game/Assets/Behaviours/TriggerEventsBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    public float delayTime = 1f;
+    public WaitForSeconds waitObj;
+
+    private void Start()
+    {
+        waitObj = new WaitForSeconds(delayTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    { 
+        yield return waitObj; 
+        triggerEnterEvent.Invoke();
+    }
+}
