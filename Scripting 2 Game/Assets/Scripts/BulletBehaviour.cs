e218667f8f7c6d8721903dd71c88240f2daf2ad9
Megan Petersen <petersen.megan9646@gmail.com>
2020-10-21 18:22:04 -0600

    Followed along in class

---
+++ Scripting 2 Game/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class BulletBehaviour : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public float bulletForce;
+    private float lifeTime; 
+
+    private IEnumerator Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        
+        rBody.AddRelativeForce(Vector3.forward * bulletForce);
+        yield break;
+        //yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
