6109d0ba8d9691a776f03901aa08b760fd9f2776
Megan Petersen <petersen.megan9646@gmail.com>
2020-11-06 23:44:12 -0700

    Created and scripted an animation script, but it's not working. :(

---
+++ Scripting 2 Game/Assets/Scripts/Animations.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Animations : MonoBehaviour
+{
+    private Animator anim;
+    private AnimationClip walk;
+    private AnimationClip idle; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        anim.SetBool("walk", false);
+
+        anim.SetBool("Idle", true); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+           
+            anim.SetBool("Walk", true);
+ 
+        }
+
+    }
+        
+}
+
+    
