a45ea934b7a3ca328e2b2ee23331b269068be39f
Megan Petersen <petersen.megan9646@gmail.com>
2020-11-15 22:38:58 -0700

    Working on a Solution to Expand my Door Puzzle for Marty's Game

---
+++ Scripting 2 Game/Assets/Behaviours/Puzzles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzles : MonoBehaviour
+{
+    public IntData myCounter;
+   
+   
+    private void Start()
+    {
+        myCounter.value = 0; 
+    }
+
+    private void Update()
+    {
+        if (myCounter.value >= 4)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+    }
+    
+    
+}
+
+
+
+
+
