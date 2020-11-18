8080847876eb78f3faeec98991ddcedaa85e53c2
Megan Petersen <petersen.megan9646@gmail.com>
2020-11-18 09:06:49 -0700

    Forgot to commit my AI Brains

---
+++ Scripting 2 Game/Assets/Behaviours/AI/AIBrainBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+[CreateAssetMenu]
+public class AIBrainBase : ScriptableObject
+{
+    public float health;
+    public float speed;
+    public GameObject art; 
+
+    public void Navigate(NavMeshAgent agent)
+    {
+        
+    }
+
+
+
+}
---
+++ Scripting 2 Game/Assets/Behaviours/AI/AIWithBrains.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIWithBrains : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public AIBrainBase aiBrain; 
+    public Transform player;
+    private int i = 0; 
+    public List<brainUpgrades>
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aiBrain.Navigate(agent);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        aiBrain = brainUpgrades[i];
+    }
+}
